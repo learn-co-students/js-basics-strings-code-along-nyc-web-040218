0d48f1863f649b38a1296f0869f7d112b15fabf0
github username <ianjpollack@gmail.com>
2018-05-14 13:30:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+var currentUser = "Ian"
+//var welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+var welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+var excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+//var shortGreeting = `Welcome, ${currentUser[0]}!`
+var shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
