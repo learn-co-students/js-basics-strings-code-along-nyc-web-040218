d02f9475d741f8d765e47edd00fd74913a557264
Ryan Hu <ryanhuuu@gmail.com>
2018-05-14 16:04:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+let currentUser = "bob";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
